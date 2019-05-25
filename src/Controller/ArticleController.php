0a7783e0a00f9205c8252b654eed4d6e7a6cf5b7
Andrii Pospielov <andriypospielov@gmail.com>
2019-05-25 11:56:18 +0300

    fixed TIG added more symfony stuff

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,8 +16,17 @@ class ArticleController extends AbstractController
      * @param ArticleRepository $articleRepository
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function index(ArticleRepository $articleRepository)
+    public function index(ArticleRepository $articleRepository, EntityManagerInterface $entityManager)
     {
+
+//        $article = new Article();
+//        $article->setContent(md5(random_bytes(666)));
+//        $article->setTitle(md5(random_bytes(666)));
+//
+//        $entityManager->persist($article);
+//        $entityManager->flush();
+
+
         $articles = $articleRepository->findAll();
         return $this->json([
             'articles' => $articles
